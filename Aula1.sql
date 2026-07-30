e7ed03c635d9275536a643f0d37d151ea01b40cf
Pedro Souza <pedroelucassouza@gmail.com>
2026-07-30 13:50:57 -0300

    Código Corrigido

--- Aula1.sql
+++ Aula1.sql
@@ -1,21 +1,25 @@
 USE CursoPraticoDB;
 GO
 
--- 1. Tabela de Catecorias
+DROP TABLE IF EXISTS Produtos;
+DROP TABLE IF EXISTS Categorias;
+GO
+
+-- 1. Tabela de Categorias
 CREATE TABLE Categorias (
     CategoriaID INT IDENTITY(1, 1) PRIMARY KEY,
     NomeCategoria VARCHAR(50) NOT NULL
 );
 GO
 
--- 2. Tabela de Produtos(com FK referenciado Categoria)
+-- 2. Tabela de Produtos (com FK referenciando Categorias)
 CREATE TABLE Produtos (
     ProdutoID INT IDENTITY(1, 1) PRIMARY KEY,
     NomeProduto VARCHAR(100) NOT NULL,
     Preco DECIMAL(10,2) CHECK (Preco >= 0),
     Estoque INT DEFAULT 0,
     CategoriaID INT,
-    CONSTRAINT FK_Produtos_Categorias FOREIGN KEY (CategoriasID)
-        REFERENCES Categorias(CategoriasID)
+    CONSTRAINT FK_Produtos_Categorias FOREIGN KEY (CategoriaID)
+        REFERENCES Categorias(CategoriaID)
 );
 GO
